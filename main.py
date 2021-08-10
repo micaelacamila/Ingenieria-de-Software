dbf62ee34766fb8cb3e772dc1c16391fd324333a
Micaela Patiño <micaela.patino@ucb.edu.bo>
2021-08-09 22:11:17 -0400

    Añadimos el dia

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-﻿hora = int(input("Hora de inicio (horas): "))
+﻿dia = input("Ingresa el dia de inicio ")
+hora = int(input("Hora de inicio (horas): "))
 min = int(input("Minuto de inicio (minutos): "))
 dura = int(input("Duración del evento (minutos): "))
 
@@ -9,4 +10,5 @@ new_m = new_m%60
 new_h = new_h+hora
 if(new_h>=24):
     new_h=new_h%24
-print(new_h,":",new_m)
+print("El evento inicia el día ",dia, "a las ",hora,":",min);
+print("\n Y finaliza: ",new_h,":",new_m)
