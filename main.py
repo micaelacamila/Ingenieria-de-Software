f908c022be7973bf528ef0914575588b6e4ab0f1
Micaela Patiño <micaela.patino@ucb.edu.bo>
2021-08-09 22:04:28 -0400

    Empiezo a crear

---
+++ main.py
@@ -0,0 +1,12 @@
+﻿hora = int(input("Hora de inicio (horas): "))
+min = int(input("Minuto de inicio (minutos): "))
+dura = int(input("Duración del evento (minutos): "))
+
+# coloca tu código aqui
+new_m = min + dura
+new_h = int(new_m / 60)
+new_m = new_m%60
+new_h = new_h+hora
+if(new_h>=24):
+    new_h=new_h%24
+print(new_h,":",new_m)
